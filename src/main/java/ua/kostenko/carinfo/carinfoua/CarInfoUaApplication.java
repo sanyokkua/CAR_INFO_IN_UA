e8cddaa3009b311182d0f47eb76727ca39e2921a
Alexander Kostenko <kostalek@us.ibm.com>
2018-10-01 11:21:11 +0300

    Initial commit

---
+++ src/main/java/ua/kostenko/carinfo/carinfoua/CarInfoUaApplication.java
@@ -0,0 +1,12 @@
+package ua.kostenko.carinfo.carinfoua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInfoUaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInfoUaApplication.class, args);
+	}
+}
