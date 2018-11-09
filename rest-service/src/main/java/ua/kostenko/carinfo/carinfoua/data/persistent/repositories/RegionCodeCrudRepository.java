ebe39926ac8e575d12fc79ee3d85c05af30fa084
Alexander Kostenko <kostalek@us.ibm.com>
2018-11-09 15:42:47 +0200

    Removed several files

--- rest-service/src/main/java/ua/kostenko/carinfo/carinfoua/data/persistent/repositories/RegionCodeCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.kostenko.carinfo.carinfoua.data.persistent.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import ua.kostenko.carinfo.carinfoua.data.persistent.entities.RegionCodeEntity;
-
-public interface RegionCodeCrudRepository extends CrudRepository<RegionCodeEntity, String> {
-}
