99668a7b93f4759e4b09a6ecd3b10c845b095467
Alexander Kostenko <sanyokkua@gmail.com>
2018-11-12 17:36:22 +0200

    Remove unused methods

--- rest-service/src/main/java/ua/kostenko/carinfo/rest/controllers/utils/ResponseCreatorHelper.java
+++ rest-service/src/main/java/ua/kostenko/carinfo/rest/controllers/utils/ResponseCreatorHelper.java
@@ -34,40 +34,4 @@ public class ResponseCreatorHelper {
         this.serviceCenterCrudRepository = serviceCenterCrudRepository;
         this.regionCodeCrudRepository = regionCodeCrudRepository;
     }
-
-    public List<CombinedInformation> getCombinedInformation(List<RegistrationInformationEntity> registrationInformationEntityList) {
-        return registrationInformationEntityList.stream().map(registrationInformation -> {
-            log.info("Mapping RegistrationInformationEntity to CombinedInformation from object: {}", registrationInformationEntityList);
-            Auto auto = getAuto(registrationInformation);
-            log.info("Created Auto object: {}", auto);
-            AdministrativeObjectEntity administrativeObject = administrativeObjectsCrudRepository.findById(registrationInformation.getAdministrativeObjectCode()).orElse(null);
-            log.info("Found AdministrativeObjectEntity {}", administrativeObject);
-            Registration registration = getRegistration(registrationInformation, administrativeObject);
-            log.info("Created Registration object: {}", registration);
-            ServiceCenterEntity serviceCenterEntity = serviceCenterCrudRepository.findById(registrationInformation.getDepartmentCode()).orElse(null);
-            log.info("Found ServiceCenterEntity object: {}", serviceCenterEntity);
-            ServiceCenter serviceCenter = getServiceCenter(registrationInformation, serviceCenterEntity);
-            log.info("Create ServiceCenter object: {}", serviceCenter);
-            return new CombinedInformation(auto, registration, serviceCenter);
-        }).collect(Collectors.toList());
-    }
-
-    private Auto getAuto(RegistrationInformationEntity registrationInformation) {
-        return new Auto(registrationInformation.getCarBrand(), registrationInformation.getCarModel(), registrationInformation.getCarMakeYear(), registrationInformation
-                .getCarColor(),
-                        registrationInformation.getCarKind(), registrationInformation.getCarBody(), registrationInformation.getCarPurpose(), registrationInformation.getCarFuel(),
-                        registrationInformation.getCarEngineCapacity(), registrationInformation.getCarOwnWeight(), registrationInformation.getCarTotalWeight());
-    }
-
-    private Registration getRegistration(RegistrationInformationEntity registrationInformation, AdministrativeObjectEntity administrativeObject) {
-        String code = registrationInformation.getCarNewRegistrationNumber().substring(0, 2);
-        RegionCodeEntity defaultIfEmpty = new RegionCodeEntity("", "");
-        return new Registration(Registration.PersonKind.getPersonKind(registrationInformation.getPerson()), administrativeObject.getTypeName(), administrativeObject.getName(),
-                                registrationInformation.getOperationName(), registrationInformation.getRegistrationDate(), registrationInformation.getCarNewRegistrationNumber(),
-                                regionCodeCrudRepository.findById(code).orElse(defaultIfEmpty));
-    }
-
-    private ServiceCenter getServiceCenter(RegistrationInformationEntity registrationInformation, ServiceCenterEntity serviceCenterEntity) {
-        return new ServiceCenter(serviceCenterEntity.getDepId(), serviceCenterEntity.getAddress(), serviceCenterEntity.getEmail(), registrationInformation.getDepartmentName());
-    }
 }
