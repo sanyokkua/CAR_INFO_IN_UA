16c9533034cb38192c24afb8d8fb59a57df5e921
Alexander Kostenko <sanyokkua@gmail.com>
2018-11-12 10:44:42 +0200

    Moved Application to root package

--- data-consumer-service/src/main/java/ua/kostenko/carinfo/Application.java
+++ data-consumer-service/src/main/java/ua/kostenko/carinfo/Application.java
@@ -1,4 +1,4 @@
-package ua.kostenko.carinfo.consumer;
+package ua.kostenko.carinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- data-producer-service/src/main/java/ua/kostenko/carinfo/Application.java
+++ data-producer-service/src/main/java/ua/kostenko/carinfo/Application.java
@@ -1,4 +1,4 @@
-package ua.kostenko.carinfo.producer;
+package ua.kostenko.carinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
