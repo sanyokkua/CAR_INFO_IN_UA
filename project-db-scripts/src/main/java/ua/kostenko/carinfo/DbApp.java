517947733cb800878a1f3c07561d61a5bb82630d
Alexander Kostenko <sanyokkua@gmail.com>
2020-01-06 23:12:51 +0200

    Configured Liquibase scripts

---
+++ project-db-scripts/src/main/java/ua/kostenko/carinfo/DbApp.java
@@ -0,0 +1,13 @@
+package ua.kostenko.carinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbApp.class, args);
+    }
+
+}
